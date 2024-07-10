9bc9776989d5ac7e687a77e8a26d878b559f3d21
natsolivar <158124715+natsolivar@users.noreply.github.com>
2024-07-10 20:21:44 +0800

    Delete EMVS directory

--- EMVS/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('CLIENT_ID', 'd80ffbfc-4d38-4da4-a8d7-9fae52dac33c');
-define('CLIENT_SECRET', 'QPH8Q~K2SyKWQhvv7aco4wvrr9sfbhcHfUEYZdn_');
-define('REDIRECT_URI', 'http://localhost/Capstone/EMVS/signin.php');
-define('AUTHORITY', 'https://login.microsoftonline.com/06a1e867-7e6a-4299-881a-59757b9ea2c3');
-define('SCOPES', 'openid profile User.Read');
-?>
